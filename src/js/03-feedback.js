b6aa85b1f2bb8c923fa7a7bccb095106d6124fdd
Alex <afanasbev1@gmail.com>
2023-04-01 17:14:13 +0300

    finish HW-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,52 @@
+import throttle from 'lodash.throttle';
 
-const feedbackForm = document.querySelector('.feedback-form')
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {}; 
+
+// 1. Выполним делегирование по основной форме
+// 2. Вешаем слушателя события  "input - для текстовых полей, и submit -
+// для кнопки оптравки"
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextInput, 500));
+
+// 3. Вызов функции "автозаполнения из localStorage, если значение STORAGE_KEY !== null"
+populateTextArea();
+
+// 4. Реализация ф-ции onTextInput
+
+function onTextInput(event) {
+  formData[event.target.name] = event.target.value;
+  const formDataStringify = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataStringify);
+}
+
+// 5. Реализация ф-ции onFormSubmit
+function onFormSubmit(event) {
+  event.preventDefault(); // сброс дефолтных настроек браузера (отключаем перезагрузку);
+  event.target.reset(); // сброс значений полей при нажатии на кнопку
+  localStorage.removeItem(STORAGE_KEY); // удаление ключа из LocalStorage
+}
+
+// 6. Реализация ф-ции populateTextArea с проверкой на падение кода
+function populateTextArea() {
+  const messageJsonParse = localStorage.getItem(STORAGE_KEY);
+  try {
+    if (messageJsonParse) { // проверка на Null
+      messageJsonParse = JSON.parse(messageJsonParse); // если код валидный (!== null) преобразуем JSON -> JS
+      // Object.entries возвращает массив собственных перечисляемых свойств указанного объекта в формате [key, value],
+      Object.entries(messageJsonParse).forEach(
+        //Записываем значение ключей полученных из localStorage которые были преоброзаровано в JS {}.
+        ([key, value]) => {
+          formData[key] = value;
+          form[key].value = value;
+        }
+      );
+    }
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
