0c2b7e7941b27b6e02199e60610e3097f0920be6
stoletoff <82263525+stoletoff@users.noreply.github.com>
2023-04-01 14:14:39 +0000

    Deploying to gh-pages from @ stoletoff/goit-js-hw-08@b6aa85b1f2bb8c923fa7a7bccb095106d6124fdd 🚀

--- 03-feedback.0de27e7c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form");
-//# sourceMappingURL=03-feedback.0de27e7c.js.map
--- 03-feedback.41980729.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form");
-//# sourceMappingURL=03-feedback.41980729.js.map
